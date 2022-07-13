7d49f7a853c114b77801752ef0b3f9ddcc100f84
leeseul <seul9085@naver.com>
2022-07-13 15:39:41 +0900

    A+B - 4

--- beakjoon/10951.cpp
+++ beakjoon/10951.cpp
@@ -6,7 +6,7 @@ int main() {
 	int a, b;
 	//파일 끝(EOF)에 도달할 때까지 출력한다는 뜻
 	//파일 끝을 의미하는 EOF를 읽게 되면, true값으로 바뀐다.
-	while (cin >> a >> b) { //cin>>a>>b로도 가능
+	while (cin >> a >> b) { //!cin.eof()로도 가능
 		cout << a + b << endl;
 	}
 	return 0;
