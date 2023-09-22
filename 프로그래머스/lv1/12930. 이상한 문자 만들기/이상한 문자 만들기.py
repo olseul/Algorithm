2dc1fe716623fe3301d5c4fba277b50c620d4c68
olseul <72639353+olseul@users.noreply.github.com>
2023-09-22 14:14:43 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = ''
+    arr = s.split(' ')
+    for i in arr:
+        for j in range(len(i)):
+            if j % 2 == 0:
+                answer += i[j].upper()
+            else:
+                answer += i[j].lower()
+        answer+= ' '
+    return answer[0:-1]
