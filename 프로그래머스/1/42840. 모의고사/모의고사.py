01413e72d5c56b2a69ae2aa031bf1c883bae2ed3
olseul <72639353+olseul@users.noreply.github.com>
2023-10-10 14:20:54 +0900

    [level 1] Title: 모의고사, Time: 2.48 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,20 @@
+def solution(answers):
+    win = []
+    score = [0,0,0]
+    
+    student1 = [1,2,3,4,5]
+    student2 = [2,1,2,3,2,4,2,5]
+    student3 = [3,3,1,1,2,2,4,4,5,5]
+    
+    for i in range(len(answers)) :
+        if answers[i] == student1[i%5] :
+            score[0] += 1
+        if answers[i] == student2[i%8] :
+            score[1] += 1
+        if answers[i] == student3[i%10] :
+            score[2] += 1
+        
+    for i in range(3):
+        if score[i] == max(score):
+            win.append(i+1)
+    return win
