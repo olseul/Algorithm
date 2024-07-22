dfca1c6142c41893689d09e05a8380638cab8d04
이슬 <72639353+olseul@users.noreply.github.com>
2024-07-22 18:24:18 +0900

    [level 0] Title: 코드 처리하기, Time: 14.61 ms, Memory: 39.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,23 @@
+function solution(code) {
+    let mode = 0;
+    let ret = "";
+    for(let i = 0; i < code.length; i++){
+        if(mode == 0){
+            if(code[i] !== "1" && i%2 == 0){
+                ret += code[i];
+            }
+            else if(code[i] == "1"){
+                mode = 1;
+            }
+        }
+        else if(mode == 1){
+            if(code[i] !== "1" && i%2 !== 0){
+                ret += code[i];
+            }
+            else if(code[i] == "1"){
+                mode = 0;
+            }
+        }
+    }
+    return ret.length ? ret : "EMPTY";
+}
