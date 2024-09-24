c23d37f4690e9155674c8cca473569053c6133b5
이슬 <72639353+olseul@users.noreply.github.com>
2024-09-24 21:20:57 +0900

    [level 1] Title: 콜라 문제, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -0,0 +1,9 @@
+function solution(a, b, n) {
+    let result = 0; 
+    while(n >= a){
+        result += Math.floor(n / a) * b;
+        n = Math.floor(n / a) * b + n % a;
+    }
+    
+    return result;
+}
