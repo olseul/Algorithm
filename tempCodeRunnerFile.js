f185d4db4f059cda0af16bfb416531f9fd35e8a7
olseul <seul9085@naver.com>
2025-04-20 20:46:24 +0900

    백준 17204 죽음의 게임

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,21 +1,39 @@
 const fs = require("fs");
-const input = +fs.readFileSync("/dev/stdin").toString().trim();
+const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
+const [n, k] = input[0].split(" ").map(Number);
+let visited = Array(n - 1).fill(false);
+const arr = input.slice(1).map(Number);
+let m = 0;
+let find = false;
 
-const dp = Array(input + 1).fill(0); // dp[i] = i를 1로 만들기 위한 최소 연산 횟수
-
-for (let i = 2; i <= input; i++) {
-  // 기본적으로 1을 빼는 경우
-  dp[i] = dp[i - 1] + 1; // dp[5] = dp[4] + 1(4의 최소연산횟수 + 1)
-
-  // 2로 나눠지는 경우
-  if (i % 2 === 0) {
-    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+function dfs(value) {
+  visited[value] = true;
+  let current = arr[value];
+  if (arr[current] == k) {
+    find = true;
   }
-
-  // 3으로 나눠지는 경우
-  if (i % 3 === 0) {
-    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  if (!visited[current]) {
+    m += 1;
+    dfs(current);
+  }
+  if (visited[current]) {
+    return false;
   }
 }
 
-console.log(dp[input]);
+for (let i = 0; i < n; i++) {
+  if (!visited[i]) {
+    if (find) {
+      break;
+    }
+    if (!dfs(i)) {
+      break;
+    }
+  }
+}
+
+if (find) {
+  console.log(m);
+} else {
+  console.log(-1);
+}
