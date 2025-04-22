8176ec8c9c5fed7cfe804ecd98d2efee4411ed2b
이슬 <72639353+olseul@users.noreply.github.com>
2025-04-22 21:04:55 +0900

    [Silver II] Title: 촌수계산, Time: 92 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const n = +input[0];
+const [a, b] = input[1].split(" ").map(Number);
+const m = +input[2];
+let graph = Array.from({ length: n + 1 }, () => []);
+let visited = Array(n + 1).fill(false);
+let count = 0;
+
+for (let i = 1; i <= m; i++) {
+  const [x, y] = input[i + 2].split(" ").map(Number);
+
+  graph[x].push(y);
+  graph[y].push(x);
+}
+
+function dfs(value) {
+  visited[value] = true;
+
+  for (const next of graph[value]) {
+    if (!visited[next]) {
+      count += 1;
+      if (next == b) {
+        console.log(count);
+        return true;
+      }
+      if (dfs(next)) {
+        return true;
+      }
+      count -= 1;
+    }
+  }
+  return false;
+}
+
+if (!dfs(a)) {
+  console.log(-1);
+}
