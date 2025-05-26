3c620d372d643ba4ccda04b304233c89499b8fc0
이슬 <72639353+olseul@users.noreply.github.com>
2025-05-26 23:23:46 +0900

    [level 0] Title: 문자 개수 세기, Time: 3.91 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,16 +1,17 @@
-function isUpperCase(str){
-    return str === str.toUpperCase();
-}
-
-
 function solution(my_string) {
-    let result = new Array(52).fill(0);
-    for(let element of my_string){
-        if(isUpperCase(element)){
-            result[element.charCodeAt() - 65] += 1;
+    let arr = new Array(52).fill(0);
+    
+    
+    
+    //a는 97 z는 122 A는 65 Z는 90
+    console.log('z'.charCodeAt(0));
+    my_string.split('').forEach(a => {
+        let asc = a.charCodeAt(0);
+        if(97 <= asc && asc <= 122){
+            arr[a.charCodeAt(0) - 97 + 26] += 1;
         }else{
-            result[element.charCodeAt() - 71] += 1;
+            arr[a.charCodeAt(0) - 65] += 1;
         }
-    }
-    return result;
+    })
+    return arr;
 }
