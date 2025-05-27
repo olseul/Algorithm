c7164851474834578c45a62ac71dce4852d57c1a
이슬 <72639353+olseul@users.noreply.github.com>
2025-05-27 22:41:31 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 3.85 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,11 +1,8 @@
 function solution(arr, queries) {
-    let result = arr.slice();
-     for (let i = 0; i < queries.length; i++) {
-        let s = queries[i][0]; // 시작 인덱스
-        let e = queries[i][1]; // 끝 인덱스
-        for (let j = s; j <= e; j++) { // 시작 인덱스부터 끝 인덱스까지 반복
-            result[j] += 1; // 해당 범위의 result 배열 요소를 1씩 증가
+    queries.forEach(([s,e])=>{
+        for(let i=s; i<=e; i++){
+            arr[i] += 1;
         }
-    }
-    return result;
+    })
+    return arr;
 }
