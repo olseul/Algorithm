28b94b8cf7c284012311185fccd20d14fb8c29ef
이슬 <72639353+olseul@users.noreply.github.com>
2025-05-27 21:42:19 +0900

    [level 0] Title: 배열 만들기 3, Time: 1.38 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,7 +1,4 @@
 function solution(arr, intervals) {
-    let result = [];
-    intervals.forEach(([a,b])=>{
-        result = result.concat(arr.slice(a,b+1));
-    })
-    return result;
+    var answer = [];
+    return answer;
 }
