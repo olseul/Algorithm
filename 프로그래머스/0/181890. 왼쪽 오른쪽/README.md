68ac40bfd5e2a1ad9a1e64667ce6805f060d30a4
이슬 <72639353+olseul@users.noreply.github.com>
2025-05-27 22:15:50 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
@@ -1,10 +1,10 @@
 # [level 0] 왼쪽 오른쪽 - 181890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 1.88 ms
+메모리: 33.5 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 12일 22:30:53
+2025년 05월 27일 22:15:48
 
 ### 문제 설명
 
--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,24 +1,17 @@
 function solution(str_list) {
-    var answer = [];
-    l_idx = str_list.indexOf("l");
-    r_idx = str_list.indexOf("r");
+    let a = str_list.indexOf('l');
+    let b = str_list.indexOf('r');
     
-    if(l_idx == -1){
-        l_idx = 200;
-    }if(r_idx == -1){
-        console.log(r_idx)
-        r_idx = 200;
+    if(a === -1 && b === -1){
+        return [];
     }
     
-    console.log(`${r_idx} ${l_idx}`);
-    if(l_idx < r_idx){
-        console.log("여기 오니?")
-        answer = str_list.slice(0, l_idx);
-    }else if(l_idx > r_idx){
-        answer = str_list.slice(r_idx + 1);
+    if(a === -1) a = 2000;
+    if(b === -1) b = 2000;
+    
+    if(a<b){
+        return str_list.slice(0, a);
+    }else{
+        return str_list.slice(b+1);
     }
-    else if(l_idx == 200 && r_idx == 200){
-        answer = [];
-    }
-    return answer;
 }
