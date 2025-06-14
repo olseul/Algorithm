e437476a430f45217c5ccf646f1897e915e51e3a
이슬 <72639353+olseul@users.noreply.github.com>
2025-06-14 23:33:40 +0900

    [level 2] Title: 카펫, Time: 7.75 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,11 +1,22 @@
 function solution(brown, yellow) {
-    let total = brown + yellow;
-    for(let h = 3; h <= Math.sqrt(total); h++){
-        if(total % h == 0){
-            let w = total / h;
-            if((h - 2) * (w - 2) == yellow){
-                return [w, h];
-            }
+    let n = brown + yellow;
+    let answer = [];
+    
+    for(let i = 1; i <= n; i++){
+        if(n % i === 0){
+            answer.push(i)
         }
     }
+    
+    
+    let arr = [];
+    for(let i=0; i<Math.floor(answer.length/2); i++){
+        arr.push([answer[answer.length-1-i], answer[i]])
+    }
+    if(answer.length%2){
+        arr.push([answer[Math.floor(answer.length/2)], answer[Math.floor(answer.length/2)]])
+    }
+    
+    let result = arr.filter(a=> yellow == (a[0] - 2) * (a[1] - 2));
+    return result[0]
 }
