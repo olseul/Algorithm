1ca1ef095dc05f0aaeb6f1fe4ca13bed5346b319
이슬 <72639353+olseul@users.noreply.github.com>
2025-06-19 22:39:06 +0900

    [level 2] Title: [3차] 압축, Time: 1796.34 ms, Memory: 40 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -0,0 +1,28 @@
+function solution(msg) {
+    let map = new Map();
+
+    for (let i = 65; i <= 90; i++) {
+        map.set(String.fromCharCode(i), i-64);
+    } 
+    let nextIndex = 27;
+    let result = [];
+    let arr = msg.split('');
+    for(let i=0; i<arr.length; i++){
+        let Yes = false;
+        for(let j=arr.length - 1; j>=i; j--){
+            let ex = arr.slice(i, j+1).join('');
+            if(map.get(ex) !== undefined){
+                result.push(map.get(ex));
+                // 수정된 부분!
+                if (j + 1 < arr.length) {
+                    map.set(ex + arr[j + 1], nextIndex++);
+                }
+
+                i+=ex.length - 1;
+                break;
+            }
+        }
+    }
+    return result;
+    
+}
